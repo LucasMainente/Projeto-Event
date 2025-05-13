d5db41a97cac74adcce7cfcc136720ea17f051cb
Lucas <lucas.m.ferreira16@aluno.senai.br>
2025-05-13 08:16:18 -0300

    first commit

---
+++ event/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+// import CadastroTipoDeEvento from './pages/cadastroTipoDeEvento/CadastroTipoDeEvento';
+// import Login from './pages/login/Login';
+// import Header from './components/header/Header';
+
+import Rotas from './Routes/routes';
+
+function App() {
+  return (
+    <Rotas/>
+    
+  )
+}
+
+export default App;
